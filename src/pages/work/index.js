c90f91fcf3b935f751a6efeaf523143d0b76020f
Vinicius Berro <vini.berro@gmail.com>
2023-11-07 18:08:44 -0300

    adicionado informacao do usuario

--- src/pages/work/index.js
+++ src/pages/work/index.js
@@ -1,5 +1,7 @@
 import Header from "../../component/header";
-import { useState } from "react";
+import { useState,useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../contexts/auth";
 import { addDoc,collection } from "firebase/firestore";
 import { db } from "../../db/Firebase";
 
@@ -7,14 +9,19 @@ import { Card,Form,Button,DropdownButton,Dropdown,FloatingLabel } from "react-bo
 import './work.css'
 
 export default function Work(){
-    const [nome,setNome] = useState('')
+    const navigate = useNavigate();
+
+    const {user} = useContext(AuthContext)
+    
+    const [nome,setNome] = useState(user? user.nome:'')
     const [pedido, setPedido] = useState('')
     const [telefone, setTelefone]=useState('')
-    const [rua, setRua] = useState('')
-    const [bairro, setBairro] = useState('')
-    const [numero, setNumero] = useState('')
-    const [comple, setComple] = useState('')
+    const [rua, setRua] = useState(user? user.rua:'')
+    const [bairro, setBairro] = useState(user? user.bairro:'')
+    const [numero, setNumero] = useState(user? user.numero: '')
+    const [comple, setComple] = useState(user? user.comple: '')
 
+    
 
 
     function selecionar(e){
@@ -30,7 +37,7 @@ export default function Work(){
         rua !=='' && 
         bairro !== '' &&
         numero !== ''){
-          await addDoc(collection(db,'pedido'), {
+          await addDoc(collection(db,'Pedido'), {
             nomeUser: nome,
             telefone: telefone,
             ruaPedido: rua,
@@ -41,11 +48,49 @@ export default function Work(){
           })
           .then(()=>{
             alert("Pedido efetuado com sucesso");
-            
+            navigate('/')
           }).catch((erro)=>{
             alert("Erro: "+erro)
           })
-        }else{
+        }else
+        if(user && !user.bairro && !user.rua && !user.numero && telefone !== '' && pedido !== ''){
+            await addDoc(collection(db,'Pedido'), {
+                nomeUser: user.nome,
+                telefone: telefone,
+                ruaPedido: rua,
+                bairroPedido: bairro,
+                numeroPedido: numero,
+                serviço: pedido,
+                comple: comple, 
+              })
+              .then(()=>{
+                alert("Pedido efetuado com sucesso");
+                navigate('/')
+              }).catch((erro)=>{
+                alert("Erro: "+erro)
+              })
+        }
+
+        else
+        if(user && telefone !== '' && pedido !== ''){
+            await addDoc(collection(db,'pedido'), {
+                nomeUser: user.nome,
+                telefone: telefone,
+                ruaPedido: user.rua,
+                bairroPedido: user.bairro,
+                numeroPedido: user.numero,
+                serviço: pedido,
+                comple: user.comple, 
+              })
+              .then(()=>{
+                alert("Pedido efetuado com sucesso");
+                navigate('/')
+              })
+              .catch((erro)=>{
+                alert("Erro: "+erro)
+              })
+        }
+        else{
             alert("Prencha todos os campos obrigatorios")
         }
     }
@@ -59,7 +104,7 @@ export default function Work(){
                 <Card.Body>
                     <Form>
                         <Form.Group className="mb-3" controlId="formGroupEmail">
-                            <Form.Control type="text" placeholder="Nome" onChange={(e)=>{setNome(e.target.value)}}/>
+                            <Form.Control type="text" placeholder="Nome" value={user?user.nome: nome} onChange={(e)=>{setNome(e.target.value)}}/>
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="formGroupEmail">
                             <Form.Control type="tel" placeholder="Telefone" onChange={(e)=>{setTelefone(e.target.value)}}/>
@@ -74,16 +119,16 @@ export default function Work(){
                           <option value="Manutenção de Pracinha">Manutenção de Pracinha</option>
                         </Form.Select>
                         <Form.Group className="mb-3" controlId="formGroupEmail">
-                            <Form.Control type="text" placeholder="Rua" onChange={(e)=>{setRua(e.target.value)}}/>
+                            <Form.Control type="text" placeholder="Rua" value={user && user.rua !== ''?user.rua: rua} onChange={(e)=>{setRua(e.target.value)}}/>
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="formGroupEmail">
-                            <Form.Control type="text" placeholder="Bairro" onChange={(e)=>{setBairro(e.target.value)}}/>
+                            <Form.Control type="text" placeholder="Bairro" value={user && user.bairro !== ''?user.bairro: bairro} onChange={(e)=>{setBairro(e.target.value)}}/>
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="formGroupEmail">
-                            <Form.Control type="number" placeholder="Numero da casa" onChange={(e)=>{setNumero(e.target.value)}}/>
+                            <Form.Control type="number" placeholder="Numero da casa" value={user && user.numero !== ''?user.numero : numero} onChange={(e)=>{setNumero(e.target.value)}}/>
                         </Form.Group>
                         
-                        <Form.Control as="textarea" placeholder="Complemento" onChange={(e)=>{setComple(e.target.value)}}/>
+                        <Form.Control as="textarea" placeholder="Complemento" value={user&& user.comple !== '' ?user.comple: comple} onChange={(e)=>{setComple(e.target.value)}}/>
                         
                         <Button variant="primary" onClick={pedir}>Fazer pedido</Button>
                     </Form>
